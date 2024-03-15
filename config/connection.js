d7a5a6ce6655a83d30dc3d11622e3ab81f53f412
sebjustseb <sebjustseb@gmail.com>
2024-03-15 02:17:16 -0700

    bewilderment

--- config/connection.js
+++ config/connection.js
@@ -18,4 +18,14 @@ if (process.env.JAWSDB_URL) {
   );
 }
 
-module.exports = sequelize;
+// Test the database connection
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+module.exports = { sequelize, testConnection };
--- seeds/seed.js
+++ seeds/seed.js
@@ -3,6 +3,7 @@ const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -15,13 +16,6 @@ const seedDatabase = async () => {
   for (const post of postData) {
     await Post.create({
       ...post,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  };
-
-  for (const comment of commentData) {
-    await Comment.create({
-      ...comment,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
